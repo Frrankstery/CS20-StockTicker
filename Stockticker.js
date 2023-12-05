047b33813f6f93269bbe7c645f2f70781aaae3d1
Yunzhe Li <55543424+Frrankstery@users.noreply.github.com>
2023-12-05 15:44:48 -0500

    Update Stockticker.js

--- Stockticker.js
+++ Stockticker.js
@@ -4,3 +4,13 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
 }).listen(8080);
+
+
+{
+  "name": "stock-ticker-info",
+  "description": "a really cool app",
+  "version": "1.0.0",
+  "engines": {
+    "node": "20.x"
+  }
+}
