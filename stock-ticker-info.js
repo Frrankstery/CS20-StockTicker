16647a28239160cfce60534af6db8b3481e3cf71
Yunzhe Li <55543424+Frrankstery@users.noreply.github.com>
2023-12-07 13:06:20 -0500

    Update stock-ticker-info.js

--- stock-ticker-info.js
+++ stock-ticker-info.js
@@ -1,3 +1,27 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+// specify the path of the CSV file
+const path = "./companies-1.csv";
+
+// Create a readstream
+// Parse options: delimiter and start from line 1
+
+fs.createReadStream(path)
+  .pipe(parse({ delimiter: ",", from_line: 1 }))
+  .on("data", function (row) {
+    // executed for each row of data
+    console.log(row);
+  })
+  .on("error", function (error) {
+    // Handle the errors
+    console.log(error.message);
+  })
+  .on("end", function () {
+    // executed when parsing is complete
+    console.log("File read successful");
+  });
+
 var http = require('http');
 var port = process.env.PORT || 3000;
 
