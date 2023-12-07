48167c037cee9c32bced3f25e8282db463a0cca6
Yunzhe Li <55543424+Frrankstery@users.noreply.github.com>
2023-12-07 12:56:41 -0500

    Update stock-ticker-info.js

--- stock-ticker-info.js
+++ stock-ticker-info.js
@@ -1,8 +1,12 @@
 var http = require('http');
+var port = process.env.PORT || 3000;
 
+console.log("this goes to the console window");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+  res.write("<h2>hello world</h2>");
+  res.write("success! This app is deployed online");
+  res.end();
+}).listen(port);
 
 
